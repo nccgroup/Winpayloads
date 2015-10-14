636953b770dafa0e1b7e17c0c19a93cdce7d767f
Charliedean <dean.charlie07@gmail.com>
2015-10-14 16:57:24 +0100

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-print '[*] Installing Wine'
+echo '[*] Installing Wine'
 apt-get update
 apt-get install wine -y
-print '[*] Installing Python Requirements'
+echo '[*] Installing Python Requirements'
 pip install blessings
-print '[*] Git Cloning Pyinstaller Into /opt'
+echo '[*] Git Cloning Pyinstaller Into /opt'
 git clone https://github.com/pyinstaller/pyinstaller.git /opt/
-print '[*] Downloading Python27 For Wine'
+echo '[*] Downloading Python27 For Wine'
 wget https://www.python.org/ftp/python/2.7.10/python-2.7.10.msi
 wine msiexec python-2.7.10.msi
-print '[*] Setting Up Shellter'
-mkdir ./shellter
-cd ./shellter
-wget https://www.shellterproject.com/Downloads/Shellter/Latest/shellter.zip
+echo '[*] Setting Up Shellter'
+mkdir shellter
+cd shellter
+wget --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0" "https://www.shellterproject.com/Downloads/Shellter/Latest/shellter.zip"
 unzip shellter
 rm -r icon
 rm -r shellcode_samples
 rm -r faq.txt
 rm -r readme.txt
 rm -r version_history.txt
-print '[*] Done'
+echo '[*] Done'
