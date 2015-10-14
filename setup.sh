f69a033c80dd5286d17a2701cb0ce020e71eb594
Charliedean <dean.charlie07@gmail.com>
2015-10-14 17:08:39 +0100

    Better be done this time

--- setup.sh
+++ setup.sh
@@ -10,10 +10,9 @@ echo '[*] Downloading Python27 For Wine'
 wget https://www.python.org/ftp/python/2.7.10/python-2.7.10.msi
 wine msiexec /i python-2.7.10.msi
 echo '[*] Setting Up Shellter'
-mkdir shellter
-cd shellter
 wget --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0" "https://www.shellterproject.com/Downloads/Shellter/Latest/shellter.zip"
 unzip shellter
+cd shellter
 rm -r icon
 rm -r shellcode_samples
 rm -r faq.txt
