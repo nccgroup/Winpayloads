6ca606b6aad3e951ca24abecd6ef1866f647696e
Charlie <Charlie Dean>
2019-06-25 11:22:38 +0100

    asyncio.sleep works, need to fix that

--- lib/listener3.py
+++ lib/listener3.py
@@ -20,48 +20,44 @@ class StartAsync(threading.Thread):
         self.listener = self.loop.run_until_complete(self.coro)
         self.loop.run_forever()
 
-    def close(self):
-        self.listener.close()
+    def stop(self):
+        self.server.close()
         self.loop.call_soon_threadsafe(self.loop.stop)
         self.join()
         print('Server Stopped')
 
 
 class Client():
-    def __init__(self, writer, reader):
+    def __init__(self, writer, reader, addr):
+        self.addr = addr
         self.writer = writer
         self.reader = reader
         self.in_buffer = []
         self.out_buffer = []
 
-    async def recieve(self):
+    async def receive(self):
         print("started reading")
         while True:
             data = await self.reader.read(8000)
             print("read data")
-            if data:
-                if data == b'':
-                    print('Received EOF. Client disconnected.')
-                    return
-                else:
-                    self.in_buffer.append(data.decode())
+            if data == b'':
+                self.close_client()
+            else:
+                self.in_buffer.append(data.decode())
 
     async def send(self):
         print("started sending")
         while True:
-            data = await self.writable()
-            print("sent data")
-            self.writer.write(data.encode())
-            await self.writer.drain()
-
-    async def writable(self):
-        while True:
             if len(self.out_buffer) > 0:
-                return self.out_buffer.pop()
+                data = self.out_buffer.pop()
+                print("sent data")
+                self.writer.write(data.encode())
+                await self.writer.drain()
+            await asyncio.sleep(0.2)
 
-    async def close_client(self):
+    def close_client(self):
         self.writer.close()
-        self.reader.close()
+        print("Closing Client {}".format(self.addr))
 
 
 class Server():
@@ -73,13 +69,18 @@ class Server():
         addr = client_writer.get_extra_info('peername')
         print('Client connected: {}'.format(addr))
 
-        client = Client(client_writer, client_reader)
+        client = Client(client_writer, client_reader, addr)
         self.clientnumber += 1
         self.clients[self.clientnumber] = client
         await asyncio.gather(
-            client.recieve(),
-            client.send()
+            client.send(),
+            client.receive()
+
         )
+
+    def close(self):
+        for client in self.clients.values():
+            client.close_client()
 
 
 if __name__ == '__main__':
@@ -96,4 +97,4 @@ if __name__ == '__main__':
             if 'send' in comm:
                 listener.server.clients[1].out_buffer.append(comm.split()[1])
     except KeyboardInterrupt:
-        listener.close()
+        listener.stop()
